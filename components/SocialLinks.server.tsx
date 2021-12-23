74d6b4c9d5ba1cd2160757bb5223f37ba4f93068
Jamie McElwain <jgmcelwain@icloud.com>
2021-12-23 21:14:28 +0000

    refactor: extract focus ring to its own utility class

--- components/SocialLinks.server.tsx
+++ components/SocialLinks.server.tsx
@@ -31,7 +31,7 @@ export default function SocialLinks() {
             href={socialLink.url}
             target='_blank'
             rel='noreferrer'
-            className='w-full z-20 relative bg-white dark:bg-black text-black hover:text-blue-500 dark:text-green-400 dark:hover:text-green-200 p-2 border border-black dark:border-green-400 rounded-sm flex justify-center items-center transition-all ease-out transform-gpu translate-x-0 translate-y-0 group-hover:-translate-x-[1px] group-hover:-translate-y-[1px] focus:outline-none focus:ring-2 ring-blue-500 dark:ring-green-200 ring-offset-2 ring-offset-white dark:ring-offset-black'
+            className='w-full z-20 relative bg-white dark:bg-black text-black hover:text-blue-500 dark:text-green-400 dark:hover:text-green-200 p-2 border border-black dark:border-green-400 rounded-sm flex justify-center items-center transition-all ease-out transform-gpu translate-x-0 translate-y-0 group-hover:-translate-x-[1px] group-hover:-translate-y-[1px] focus-ring'
           >
             {cloneElement(socialLink.icon, { size: 18 })}
 
