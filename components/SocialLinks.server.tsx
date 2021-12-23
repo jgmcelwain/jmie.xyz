2a40fd9de0a93f8d8663c89a6c35744fbdd504b0
Jamie McElwain <jgmcelwain@icloud.com>
2021-12-23 21:07:32 +0000

    refactor: use cloneElement instead of copy pasting prop

--- components/SocialLinks.server.tsx
+++ components/SocialLinks.server.tsx
@@ -1,24 +1,24 @@
+import { cloneElement } from 'react';
 import { GitHub, Linkedin, Twitter } from 'react-feather';
 
-const ICON_SIZE = 18;
 const SOCIAL_LINKS = [
   {
     key: 'github',
     name: 'GitHub',
     url: 'https://github.com/jgmcelwain',
-    icon: <GitHub size={ICON_SIZE} />,
+    icon: <GitHub />,
   },
   {
     key: 'linkedin',
     name: 'Linkedin',
     url: 'https://www.linkedin.com/in/jamiemcelwain',
-    icon: <Linkedin size={ICON_SIZE} />,
+    icon: <Linkedin />,
   },
   {
     key: 'twitter',
     name: 'Twitter',
     url: 'https://twitter.com/jgmcelwain',
-    icon: <Twitter size={ICON_SIZE} />,
+    icon: <Twitter />,
   },
 ];
 
@@ -33,7 +33,7 @@ export default function SocialLinks() {
             rel='noreferrer'
             className='w-full z-20 relative bg-white dark:bg-black text-black hover:text-blue-500 dark:text-green-400 dark:hover:text-green-200 p-2 border border-black dark:border-green-400 rounded-sm flex justify-center items-center transition-all ease-out transform-gpu translate-x-0 translate-y-0 group-hover:-translate-x-[1px] group-hover:-translate-y-[1px] focus:outline-none focus:ring-2 ring-blue-500 dark:ring-green-200 ring-offset-2 ring-offset-white dark:ring-offset-black'
           >
-            {socialLink.icon}
+            {cloneElement(socialLink.icon, { size: 18 })}
 
             <span className='pl-2 text-sm font-medium'>{socialLink.name}</span>
           </a>
